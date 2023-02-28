5310fbacaf5cf10978e4136415c842af2ace9447
igaemas <sasha.deroubaix.dev@proton.me>
2023-02-28 16:59:20 +0100

    mise au propre et script de conf

--- auto-enum.py
+++ auto-enum.py
@@ -2,22 +2,44 @@
 import nmapthon as nm
 
 import sys
+from configparser import ConfigParser
+parser = ConfigParser()
+parser.read('config.ini')
 
 remote_ip_address = sys.argv[1]
+all_ports_number = parser.get('speed_nmap', 'ports')
+speed_nmap_arg = parser.get('speed_nmap', 'arg')
 remote_open_ports = []
 
-print("\n--- starting speed port scan ---\n")
-
-port_scan = nm.NmapScanner(remote_ip_address, arguments='-sS -Pn')
-port_scan.run()
+# 
+# fonction to do a speed enumeration of port with nmap
+#
+def speed_nmap(remote_ip_address, ports_range, nmap_arg):
+    print("\n--- starting speed port scan ---\n")
     
-for host in port_scan.scanned_hosts():
-    # Get state, reason and hostnames
-    print(f"\n{host} is {port_scan.state(host)}\n")
-    # Get scanned protocols
-    for proto in port_scan.all_protocols(host):
-        # Get scanned ports
-        print("open ports :")
-        for port in port_scan.scanned_ports(host, proto):
-            state, reason = port_scan.port_state(host, proto, port)
-            print("    " + str(port))
+    port_scan = nm.NmapScanner(
+        remote_ip_address, 
+        arguments=nmap_arg, 
+        ports=ports_range
+    )
+    port_scan.run()
+    output_file = open("output.txt", "a")
+        
+    for host in port_scan.scanned_hosts():
+        # Get state, reason and hostnames
+        print(f"\n{host} is {port_scan.state(host)}\n")
+        output_file.write(f"{host} is {port_scan.state(host)}\n")
+        
+        # Get scanned protocols
+        for proto in port_scan.all_protocols(host):
+            # Get scanned ports
+            print("open ports :")
+            output_file.write("open port :\n")
+            for port in port_scan.scanned_ports(host, proto):
+                state, reason = port_scan.port_state(host, proto, port)
+                print("    " + str(port))
+                remote_open_ports.append(str(port))
+                output_file.write("    " + str(port) + "\n")     
+
+    output_file.close()
+speed_nmap(remote_ip_address, all_ports_number, speed_nmap_arg)
